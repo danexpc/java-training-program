12a71f6b349d59d27120613b9d0a364027b5a475
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-25 11:58:46 +0300

    hello-world-mvc: Write how controller works with view and service

--- hello-world-mvc/src/ua/me/training/controller/HelloWorldController.java
+++ hello-world-mvc/src/ua/me/training/controller/HelloWorldController.java
@@ -1,7 +1,11 @@
 package ua.me.training.controller;
 
 import ua.me.training.service.HelloWorldService;
+import ua.me.training.util.InputUtils;
+import ua.me.training.util.ValidationUtil;
 import ua.me.training.view.HelloWorldView;
+
+import java.util.Optional;
 
 public class HelloWorldController {
 
@@ -12,8 +16,33 @@ public class HelloWorldController {
         view = new HelloWorldView();
         service = new HelloWorldService();
     }
-
+    
     public void proceed() {
 
+        view.printMessage(HelloWorldView.STARTER_MESSAGE);
+        view.printMessage("Input first word, please: ");
+        boolean isDone = false;
+        
+        while (!isDone) {
+            String input = InputUtils.getStringValue();
+            
+            if (ValidationUtil.isWord(input)){
+                try {
+                    Optional<String> maybeOutputString = service.processWord(input);
+                    if (maybeOutputString.isPresent()) {
+                        view.printMessage(maybeOutputString.get());
+                        isDone = true;
+                    } else {
+                        view.printMessage("Input second word, please: ");
+                    }
+                } catch (RuntimeException e) {
+                    view.printErrorMessage(e.getMessage());
+                    view.printMessage("Try again: ");
+                }
+            } else {
+                view.printErrorMessage(HelloWorldView.CONTAINS_FORBIDDEN_SYMBOLS);
+                view.printMessage("Try again: ");
+            }
+        }
     }
 }
