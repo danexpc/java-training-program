2f2208d86cdf8e3eb49f0ac9c3aac05e901a0f34
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-25 11:57:45 +0300

    hello-world-mvc: Write business logic processing input word

--- hello-world-mvc/src/ua/me/training/service/HelloWorldService.java
+++ hello-world-mvc/src/ua/me/training/service/HelloWorldService.java
@@ -1,4 +1,26 @@
 package ua.me.training.service;
 
+import ua.me.training.exception.IncorrectInputOrderException;
+import ua.me.training.exception.NotMatchesWordException;
+
+import java.util.Optional;
+
 public class HelloWorldService {
+    
+    private final String firstWord = "Hello";
+    private final String secondWord = "world!";
+    private boolean isFirstEntered = false;
+    
+    public Optional<String> processWord(String input) {
+        if (input.equals(firstWord) && !isFirstEntered) {
+            isFirstEntered = true;
+            return Optional.empty();
+        } else if (input.equals(secondWord) && isFirstEntered) {
+            return Optional.of(String.format("%s %s", firstWord, secondWord));
+        } else if (input.equals(secondWord)) {
+            throw new IncorrectInputOrderException("Please, start entering with word \"Hello\"");
+        } else {
+            throw new NotMatchesWordException("Please, check spelling the word");
+        }
+    }
 }
